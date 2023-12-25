38c39513500566eaf893a166efe372898a6c035e
Imamsubekti26 <imam0subekti@gmail.com>
2023-12-26 01:34:31 +0700

    update config

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,13 +49,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'welcome';
+$route['default_controller'] = 'view/login';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 
-$router['auth/(:any)']['GET'] = 'welcome';
-$router['dashboard']['GET'] = 'welcome';
-$router['rps/(:any)']['GET'] = 'welcome';
+$route['auth/login'] = 'view/login';
+$route['auth/register']['GET'] = 'view/register';
+$route['dashboard']['GET'] = 'view/dashboard';
+$route['rps/(:any)']['GET'] = 'view/rps';
+$route['components/(:any)']['get'] = 'view/components/$1';
 
 $route['api/auth/login'] = [
   'POST' => 'auth/login',
